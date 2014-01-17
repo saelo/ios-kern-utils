5796fed3902e81ef75a2765cb88ca635aae433eb
Samuel Groß <dev@samuel-gross.de>
2014-01-17 14:57:25 +0100

    Adapted kpatch tool to test some library functions
    kpatch will now search for the uuid string in kernel memory so it should
    be able to change it on every device.

--- tools/kpatch.c
+++ tools/kpatch.c
@@ -7,26 +7,41 @@
 #include <stdio.h>
 #include <string.h>
 
+#include <sys/sysctl.h>     // sysctlbyname
+
 #include <libkern.h>
 
-// address UUID string on iPhone 4 7.0.4
-#define KERN_UUID 0x345170
 
-
-int main()
+int main(int argc, char** argv)
 {
     vm_address_t kbase;
 
-    char* data = "writing kernel memory is fun :)";
+    if (argc < 2) {
+        printf("Usage: kpatch new-uuid\n");
+        return -1;
+    }
+
+    char uuid[0x50];
+    size_t size = 0x50;
+    memset(uuid, 0, size);
+    int ret = sysctlbyname("kern.uuid", uuid, &size, NULL, 0);
+    printf("[*] uuid: %s\n", uuid);
 
     if((kbase = get_kernel_base()) == 0) {
         printf("[!] failed to get the kernel base address");
         return -1;
     }
 
-    write_kernel(kbase + KERN_UUID, (unsigned char*)data, strlen(data)+1);
+    vm_address_t uuid_addr = find_bytes(kbase, kbase + 0x1000000, (unsigned char*)uuid, strlen(uuid));
+    if (uuid_addr == 0) {
+        printf("[!] failed to find the uuid in kernel memory\n");
+        return -1;
+    }
+    printf("[*] found uuid at 0x" ADDR "\n", uuid_addr);
 
-    // now do a "sysctl kern.uuid"
+    write_kernel(uuid_addr, (unsigned char*)argv[1], strlen(argv[1])+1);
+
+    printf("[*] done, check \"sysctl kern.uuid\"\n");
 
     return 0;
 }
