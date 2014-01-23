8476bcb19e8360a0afd6f010ea84ad0676990ca8
Samuel Groß <dev@samuel-gross.de>
2014-01-23 23:21:16 +0100

    Moved toolchain files into own subdirectory

--- toolchain/mac.cmake
+++ toolchain/mac.cmake
@@ -1,14 +1,13 @@
-
-#set your arch here or ext armv7, armv6 or arm64
+# set target architecture, possible values: armv7, armv6 or arm64
 set(IOS_TARGET_ARCH arm64)
-#set your SDK by should be ok on default instalation
+# set SDK path, should be ok on default installation
 set(CMAKE_IOS_SDK_ROOT /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS7.0.sdk)
-#set your RANLIB 
+# set path of ranlib tool
 set(RANLIB_PATH /Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/ranlib)
 
 
 
-set(CMAKE_C_COMPILER clang) 
+set(CMAKE_C_COMPILER clang)
 set(CMAKE_CXX_COMPILER clang++)
 set(CMAKE_FIND_ROOT_PATH ${CMAKE_IOS_SDK_ROOT})
 set(__CMAKE_C_COMPILER_ARG1_VAL "-arch ${IOS_TARGET_ARCH} -isysroot ${CMAKE_IOS_SDK_ROOT}")
